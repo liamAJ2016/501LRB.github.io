d5dd871291dc37a177b094482495aa8b7095415d
500LRB <145224574+500LRB@users.noreply.github.com>
2024-04-12 21:19:59 +0000

    idk

--- scripts/thing.js
+++ scripts/thing.js
@@ -1,11 +1,11 @@
 const image = document.querySelector(".woah");
 const sub = document.querySelector(".submitter")
 const text = document.querySelector(".texte")
+let api = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=";
 
 sub.addEventListener("click", updatebutton);
 
 function updatebutton() {
-    let api = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=";
     if (text.value === "") {
         return false;
     } else {
